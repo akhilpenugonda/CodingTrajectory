5225bb04e585200e1c5d68a044fdf73305edb3fb
Akhil Kumar <akhilnani14@gmail.com>
2023-10-06 23:45:58 -0500

    Create MaximumAscendingSubarray1800.js

---
+++ JavaScript-Programs/MaximumAscendingSubarray1800.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function(nums) {
+    let maxSum = 0;
+    let i = 0;
+    N = nums.length;
+    while(i < N)
+    {
+        let summer = nums[i];
+        while(i+1 < N && nums[i+1] > nums[i])
+        {
+            summer+=nums[i+1];
+            i++;
+        }
+        maxSum = Math.max(summer, maxSum);
+        i++;
+    }
+    return maxSum;
+};
