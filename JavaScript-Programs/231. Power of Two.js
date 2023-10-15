7f15433516c638bba0d9206cc97d6356737d48e9
Akhil Kumar <akhilnani14@gmail.com>
2023-10-14 20:57:57 -0500

    Create 231. Power of Two.js

---
+++ JavaScript-Programs/231. Power of Two.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if(n == 1)
+    return true;
+    if(n <= 0)
+    {
+        return false;
+    }
+    while(n >= 0)
+    {
+        if(n == 0 || n == 1)
+        {
+            return true;
+        }
+        if(n%2 != 0)
+        {
+            return false;
+        }
+        n = n/2;
+    }
+    return true;
+};
+// var isPowerOfTwo = function(n) {
+//     if (n <= 0) {
+//         return false;
+//     }
+//     return (n & (n - 1)) === 0;
+// };
