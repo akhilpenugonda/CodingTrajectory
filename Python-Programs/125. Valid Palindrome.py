5b98696d20f788628dc40f0b3f6e099d3b46a172
akhilpenugonda <akhilnani14@gmail.com>
2023-12-28 21:49:58 -0500

    Validaete palindrome and invert teiee

---
+++ Python-Programs/125. Valid Palindrome.py
@@ -0,0 +1,34 @@
+#validate if string is palindrome
+
+def isPalindrome(s):
+    s = s.lower()
+    s = ''.join(e for e in s if e.isalnum())
+    return s == s[::-1]
+
+#character wise comparision using index implementation
+def isPalindrome(s):
+    s = s.lower()
+    s = ''.join(e for e in s if e.isalnum())
+    for i in range(len(s)//2):
+        if s[i] != s[len(s)-i-1]:
+            return False
+    return True
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = len(s) - 1
+        f = 0
+        s = s.lower()
+        while l >= f:
+            if not s[l].isalnum():
+                l -= 1
+                continue
+            if not s[f].isalnum():
+                f += 1
+                continue
+            if s[l] != s[f]:
+                return False
+            l -= 1
+            f += 1
+        return True
