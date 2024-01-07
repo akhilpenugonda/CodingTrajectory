c9835340ffab5ef2fd2fdcfa076035917b287672
akhilpenugonda <akhilnani14@gmail.com>
2024-01-06 23:58:10 -0500

    Insert interval find index, append aacoringly

--- Python-Programs/57. Insert Interval.py
+++ Python-Programs/57. Insert Interval.py
@@ -5,4 +5,42 @@ class Solution(object):
         :type newInterval: List[int]
         :rtype: List[List[int]]
         """
+#         # Method 1: Brute Force
+#         # Time: O(n)
+#         # Space: O(n)
+        # intervals.append(newInterval)
+        # intervals.sort()
+        # res = []
+        # for interval in intervals:
+        #     if res == [] or res[-1][1] < interval[0]:
+        #         res.append(interval)
+        #     else:
+        #         res[-1][1] = max(res[-1][1], interval[1])
+        # return res    
+    
+#         # Method 2: Binary Search
+#         # Time: O(logn)
+#         # Space: O(n)
+        def binarySearch(intervals, newInterval):
+            low = 0
+            high = len(intervals) - 1
+            while low <= high:
+                mid = (low + high) // 2
+                if intervals[mid][0] == newInterval[0]:
+                    return mid
+                elif intervals[mid][0] < newInterval[0]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return low
+        index = binarySearch(intervals, newInterval)
+        intervals.insert(index, newInterval)
+        res = []
+        for interval in intervals:
+            if res == [] or res[-1][1] < interval[0]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+        return res
+    
         
