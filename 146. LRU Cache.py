97c2ad04f3b962a00bec5f0f07e688df299db2a6
akhilpenugonda <akhilnani14@gmail.com>
2024-01-28 23:56:45 -0500

    Fixed LRU

--- 146. LRU Cache.py
+++ 146. LRU Cache.py
@@ -1,32 +1,49 @@
-class LRUCache(object):
+class LRUCache:
 
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
-        self.cache = collections.OrderedDict()
+    class Node:
+        def __init__(self, key, value):
+            self.key = key
+            self.value = value
+            self.prev = None
+            self.next = None
+
+    def __init__(self, capacity: int):
         self.capacity = capacity
-        
+        self.cache = {}
+        # Use sentinel nodes for better handling of edge cases
+        self.left = self.Node(0, 0)
+        self.right = self.Node(0, 0)
+        self.left.next = self.right
+        self.right.prev = self.left
 
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        return self.cache.get(key, -1)
-        
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._add(node)
+        return node.value
 
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
-        self.cache[key] = value
-        
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        self.cache[key] = self.Node(key, value)
+        self._add(self.cache[key])
+        if len(self.cache) > self.capacity:
+            node = self.left.next
+            self._remove(node)
+            del self.cache[node.key]
 
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+
+    def _add(self, node):
+        p = self.right.prev
+        nxt = self.right
+        p.next = nxt.prev = node
+        node.next = nxt
+        node.prev = p
